071c52a4109a5471e486a8336c6548bd3f0fcc8e
Dhruvin <dhruvinp895@gmail.com>
2021-07-18 17:21:38 -0500

    Added server code

--- graphql-apollo-tutorial/src/index.js
+++ graphql-apollo-tutorial/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
